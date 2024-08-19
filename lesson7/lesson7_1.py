8e39bbb693c8aa90be1959d6c18e3891c1609960
w.jin1213 <110092196+Imbradley1213@users.noreply.github.com>
2024-08-19 12:51:35 +0000

    第七次提交

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,22 @@
+import random
+import pyinputplus as pyip
+min=1
+max=100
+count=0
+target=random.randint(1,100)
+print('-------猜數字遊戲-------\n')
+while True:
+    count+=1
+    keyin=pyip.inputInt(f'猜數字範圍{min}~{max}',min=min,max=max)
+    if keyin==target:
+        print(f'恭喜中獎~是{keyin}')
+        print(f'已猜了{count}次')
+        break
+    elif keyin>target:
+        print('再小點')
+        max=keyin
+    elif keyin<target:
+        print('再大點')
+        min=keyin
+    print(f'已猜了{count}次')
+print('應用程式結束')
---
+++ lesson7/lesson7_2.py
@@ -0,0 +1,26 @@
+import random
+import pyinputplus as pyip
+while True:
+    min=1
+    max=100
+    count=0
+    target=random.randint(1,100)
+    print('-------猜數字遊戲-------\n')
+    while True:
+        count+=1
+        keyin=pyip.inputInt(f'猜數字範圍{min}~{max}',min=min,max=max)
+        if keyin==target:
+            print(f'恭喜中獎~是{keyin}')
+            print(f'已猜了{count}次')
+            break
+        elif keyin>target:
+            print('再小點')
+            max=keyin
+        elif keyin<target:
+            print('再大點')
+            min=keyin
+        print(f'已猜了{count}次')
+    is_play=pyip.inputYesNo('繼續嗎?(yes,no)')
+    if is_play=='no':
+        break
+print('應用程式結束')
