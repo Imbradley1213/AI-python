56515c540e01e5b1b3cfa761fde7a5fe011bff7d
w.jin1213 <110092196+Imbradley1213@users.noreply.github.com>
2024-08-21 12:39:34 +0000

    第八次提交

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -1,23 +1,4 @@
-class Myclass:
-    @classmethod
-    def status(cls,BMI:float)->str:
-        '''
-        #param BMI:這是要傳出的值
-        #return:傳出BMI的值
-        '''
-        if BMI<18.5:
-            return('過輕')
-        elif BMI<24:
-            return('正常')
-        elif BMI<27:
-            return('過重')
-        elif BMI<30:
-            return('輕度肥胖')
-        elif BMI<35:
-            return('中度肥胖')
-        else:
-            return('重度肥胖') 
-
+from widget import Myclass
 while True: 
     name=str(input('請輸入姓名:'))
     try:
